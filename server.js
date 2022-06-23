80699cc1131162e7448ce6c5194192d22b442630
calimendoza <mbaivictor1@gmail.com>
2022-06-23 00:09:16 -0700

    added middleware

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ let db,
          collection = db.collection('movies')
       })
 
-
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
       app.listen(process.env.PORT || PORT, () => {
         console.log(`Server is running on port`)
